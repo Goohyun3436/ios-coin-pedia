e3e76cc53299c46ec167ba45f27e8e6f1c20ec50
Goohyun3436 <3436hyun@gmail.com>
2025-03-09 03:22:25 +0900

    add: RxViewController Extension

--- ios-coin-pedia/Sources/Base/BaseViewController.swift
+++ ios-coin-pedia/Sources/Base/BaseViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class BaseViewController: UIViewController {
     
@@ -29,4 +31,33 @@ class BaseViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+}
+
+//MARK: - BaseViewController LifeCycle + RxSwift
+extension Reactive where Base: BaseViewController {
+    
+    var viewDidLoad: ControlEvent<Void> {
+      let source = self.methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
+      return ControlEvent(events: source)
+    }
+
+    var viewWillAppear: ControlEvent<Bool> {
+      let source = self.methodInvoked(#selector(Base.viewWillAppear)).map { $0.first as? Bool ?? false }
+      return ControlEvent(events: source)
+    }
+    var viewDidAppear: ControlEvent<Bool> {
+      let source = self.methodInvoked(#selector(Base.viewDidAppear)).map { $0.first as? Bool ?? false }
+      return ControlEvent(events: source)
+    }
+
+    var viewWillDisappear: ControlEvent<Bool> {
+      let source = self.methodInvoked(#selector(Base.viewWillDisappear)).map { $0.first as? Bool ?? false }
+      return ControlEvent(events: source)
+    }
+    var viewDidDisappear: ControlEvent<Bool> {
+      let source = self.methodInvoked(#selector(Base.viewDidDisappear)).map { $0.first as? Bool ?? false }
+      return ControlEvent(events: source)
+    }
+    
 }
